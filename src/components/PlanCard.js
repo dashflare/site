abb0d29c7ab99898fbc634cec640e8c59787ab09
Jorge Luis Betancourt <github@jorgelbg.me>
2020-08-13 15:00:12 +0200

    Reorder information of the PlanCard component

--- src/components/PlanCard.js
+++ src/components/PlanCard.js
@@ -44,14 +44,33 @@ const PlanCard = ({ product, pos }) => {
   return (
     <div
       className={
-        "sm:flex-1 lg:flex-initial rounded-t-lg rounded-bl-lg bg-white flex flex-col px-8 py-4 w-1/3 " +
+        "sm:flex-1 lg:flex-initial rounded-t-lg rounded-bl-lg bg-white flex flex-col p-8 w-1/3 justify-between " +
         (pos === 1 ? "-mt-4 -mb-8 rounded" : "mt-4 rounded-tr-none shadow-lg")
       }
     >
-      <div className="p-8 text-3xl font-bold text-center text-gray-800">
-        {product.name}
+      <div>
+        <div className="px-8 pt-8 pb-1 text-3xl font-bold text-center text-gray-800">
+          {product.name}
+        </div>
+        <div className="flex flex-col justify-around">
+          <div className="text-center px-8 text-xl mb-8 text-gray-800">
+            {product.prices.map(price => (
+              <div className="flex flex-col justify-center">
+                <div className="flex justify-center">
+                  <span className="mr-1 mt-2 text-2xl font-medium">&euro;</span>
+                  <span className="text-5xl font-medium">
+                    {(price.unit_amount / 100).toFixed(2)}
+                  </span>
+                </div>
+                <span className="text-base -mt-1 mb-2 mt-auto text-gray-700">
+                  per month
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-      <div className="border-0 border-grey-light border-t border-solid text-sm">
+      <div className="border-0 border-grey-light border-t border-solid text-sm mb-12">
         <div className="border-0 border-grey-light border-b border-solid py-4 flex items-center">
           <svg
             viewBox="0 0 20 20"
@@ -92,29 +111,16 @@ const PlanCard = ({ product, pos }) => {
               clip-rule="evenodd"
             ></path>
           </svg>
-          Full access to the raw events (i.e logs) from your visitors
+          Full access to the raw logs from your visitors
         </div>
       </div>
-      <div className="flex flex-col justify-around h-full ">
-        <div className="text-center px-8 pt-8 text-xl mb-8 text-gray-800">
-          {product.prices.map(price => (
-            <div className="flex justify-center">
-              <span className="mr-1 mt-2 text-2xl font-medium">&euro;</span>
-              <span className="text-5xl font-thin">
-                {(price.unit_amount / 100).toFixed(2)}
-              </span>
-              <span className="mb-2 mt-auto font-medium ">/</span>
-              <span className="mb-2 mt-auto font-medium ">&nbsp;month</span>
-            </div>
-          ))}
-        </div>
-        <button
-          disabled={loading}
-          className="px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
-        >
-          Sign Up
-        </button>
-      </div>
+
+      <button
+        disabled={loading}
+        className="w-5/6 px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out self-center"
+      >
+        Sign Up
+      </button>
     </div>
   )
 }
